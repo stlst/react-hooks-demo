5a5e024b9507570f0db2f92961325be868e112a2
stlst <stlst@foxmail.com>
2019-11-23 01:22:51 +0800

    modify tests

--- src/page/DemoStatement/DayActiveComponent/SelectDate/index.test.tsx
+++ src/page/DemoStatement/DayActiveComponent/SelectDate/index.test.tsx
@@ -5,10 +5,18 @@ import React from "react";
 it("should render SelectDate", () => {
   const onChange = jest.fn();
   const wrapper = shallow(<SelectDate onChange={onChange}></SelectDate>);
-  const button = wrapper.find(".test-button");
+  const button0 = wrapper.find("button").at(0);
+  const button1 = wrapper.find("button").at(1);
+  const button2 = wrapper.find("button").at(2);
 
-  button.simulate("click");
+  button0.simulate("click");
+  button1.simulate("click");
+  button2.simulate("click");
 
-  expect(button.text()).toBe("今日");
+  expect(button0.text()).toBe("今日");
+  expect(button1.text()).toBe("昨日");
+  expect(button2.text()).toBe("明日");
   expect(onChange).toHaveBeenCalledWith("2018-10-10");
+  expect(onChange).toHaveBeenCalledWith("2018-10-09");
+  expect(onChange).toHaveBeenCalledWith("2018-10-11");
 });
--- src/page/DemoStatement/DayActiveComponent/SelectDate/index.tsx
+++ src/page/DemoStatement/DayActiveComponent/SelectDate/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 export const SelectDate = ({ onChange }: { onChange: (e: any) => void }) => {
   return (
     <div>
-      <button className="test-button" onClick={() => onChange("2018-10-10")}>
-        今日
-      </button>
+      <button onClick={() => onChange("2018-10-10")}>今日</button>
       <button onClick={() => onChange("2018-10-09")}>昨日</button>
       <button onClick={() => onChange("2018-10-11")}>明日</button>
     </div>
